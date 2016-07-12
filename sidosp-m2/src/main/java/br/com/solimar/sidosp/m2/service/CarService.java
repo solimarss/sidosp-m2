a90d38301b8b0cafbf7ffbe87cf7688a011a7f7d
Solimar <Solimar@NotSol>
2016-07-11 21:16:54 -0300

    a

---
+++ sidosp-m2/src/main/java/br/com/solimar/sidosp/m2/service/CarService.java
@@ -0,0 +1,28 @@
+package br.com.solimar.sidosp.m2.service;
+
+import java.net.URISyntaxException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import br.com.solimar.sidosp.core.domain.Coleta;
+
+@Path("/coleta")
+public class CarService {
+
+	@POST
+	@Path("/")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response insert(Coleta coleta) throws URISyntaxException {
+
+	
+
+		return Response.status(200).build();
+	}
+
+}
---
+++ sidosp-m2/src/main/java/br/com/solimar/sidosp/m2/service/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package br.com.solimar.sidosp.m2.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("service/")
+public class JaxRsActivator extends Application {
+
+}
